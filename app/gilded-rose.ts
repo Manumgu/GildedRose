e9517afea8ac62030a09cc7c4a997e38efb1138b
Manuel Moreno Guillamón <99324335+Manumgu@users.noreply.github.com>
2023-06-12 21:47:38 +0200

    [REFACTOR] OOP approach
    * refactor: refactor into if else blocks
    
    The differentiation of all item types and their respective operations is now effectively done using an if-else block
    
    * refactor: procedural refactor
    
    * refactor: oop approach
    
    * refactor: renamed classes
    
    * refactor: sulfuras fixed values

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -10,58 +10,117 @@ export class Item {
   }
 }
 
-export class GildedRose {
-  items: Array<Item>;
+export class BaseItem extends Item {
+  protected hasExpired() {
+    return this.sellIn < 0;
+  }
 
-  constructor(items = [] as Array<Item>) {
+  protected decreaseSellInUnit() {
+    this.sellIn = this.sellIn - 1;
+  }
+
+  protected isMaxQuality() {
+    return this.quality === GildedRose.MAX_QUALITY;
+  }
+
+  protected modifyItemQuality(amount: number) {
+    this.quality = this.quality + amount;
+  }
+
+  protected upgradeItemUnit() {
+    this.modifyItemQuality(1);
+  }
+
+  protected downgradeItemUnit() {
+    this.modifyItemQuality(-1);
+  }
+
+  updateQuality() {
+    if (this.quality > 0) {
+      this.downgradeItemUnit();
+    }
+
+    this.decreaseSellInUnit();
+
+    if (this.hasExpired() && this.quality > 0) {
+      this.downgradeItemUnit();
+    }
+  }
+}
+
+export class AgedBrieItem extends BaseItem {
+  updateQuality() {
+    if (!this.isMaxQuality()) {
+      this.upgradeItemUnit();
+    }
+
+    this.decreaseSellInUnit();
+
+    if (this.hasExpired() && !this.isMaxQuality()) {
+      this.upgradeItemUnit();
+    }
+  }
+}
+
+export class SulfurasItem extends BaseItem {
+  private static readonly DEFAULT_QUALITY = 80;
+
+  constructor (name: string) {
+    super(name, 0, SulfurasItem.DEFAULT_QUALITY);
+  }
+
+  updateQuality() {
+  }
+}
+
+export class BackstagePassItem extends BaseItem {
+  updateQuality() {
+    if (!this.isMaxQuality()) {
+      this.upgradeItemUnit();
+
+      if (this.sellIn <= 10 && !this.isMaxQuality()) {
+        this.upgradeItemUnit();
+      }
+
+      if (this.sellIn <= 5 && !this.isMaxQuality()) {
+        this.upgradeItemUnit();
+      }
+    }
+
+    this.decreaseSellInUnit();
+
+    if (this.hasExpired()) {
+      this.modifyItemQuality(-this.quality);
+    }
+  }
+}
+
+export class ItemFactory {
+  static createItem(name: string, sellIn: number, quality: number): BaseItem {
+    switch (name) {
+      case "Aged Brie":
+        return new AgedBrieItem(name, sellIn, quality);
+      case "Sulfuras, Hand of Ragnaros":
+        return new SulfurasItem(name);
+      case "Backstage passes to a TAFKAL80ETC concert":
+        return new BackstagePassItem(name, sellIn, quality);
+      default:
+        return new BaseItem(name, sellIn, quality);
+    }
+  }
+}
+
+export class GildedRose {
+  public static readonly MAX_QUALITY: number = 50;
+  private items: Array<BaseItem>;
+
+  constructor(items = [] as Array<BaseItem>) {
     this.items = items;
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
+    for (let item of this.items) {
+      item.updateQuality();
     }
 
     return this.items;
--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -1,4 +1,4 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { BaseItem, GildedRose, ItemFactory } from '@/gilded-rose';
 
 describe("GildedRose", () => {
     it("creates a new item correctly", () => {
@@ -8,7 +8,7 @@ describe("GildedRose", () => {
         const quality = 20;
 
         // Act
-        const item = new Item(itemName, sellIn, quality);
+        const item = ItemFactory.createItem(itemName, sellIn, quality);
 
         // Assert
         expect(item.name).toBe(itemName);
@@ -19,7 +19,7 @@ describe("GildedRose", () => {
     describe("updateQuality", () => {
         it("should degrades normal items by 1 if sellIn > 0", () => {
             // Arrange
-            const gildedRose = new GildedRose([new Item('foo', 10, 20)]);
+            const gildedRose = new GildedRose([ItemFactory.createItem('foo', 10, 20)]);
 
             // Act
             const items = gildedRose.updateQuality();
@@ -31,7 +31,7 @@ describe("GildedRose", () => {
 
         it("should degrade normal items quality twice as fast after sell by date", () => {
           // Arrange
-          const gildedRose = new GildedRose([new Item('foo', 0, 10)]);
+          const gildedRose = new GildedRose([ItemFactory.createItem('foo', 0, 10)]);
   
           // Act
           const items = gildedRose.updateQuality();
@@ -43,7 +43,7 @@ describe("GildedRose", () => {
 
         it("should increases quality of Aged Brie by 1", () => {
           // Arrange
-          const gildedRose = new GildedRose([new Item('Aged Brie', 10, 20)]);
+          const gildedRose = new GildedRose([ItemFactory.createItem('Aged Brie', 10, 20)]);
 
           // Act
           const items = gildedRose.updateQuality();
@@ -55,7 +55,7 @@ describe("GildedRose", () => {
 
         it("should not increase quality of any item above 50", () => {
           // Arrange
-          const gildedRose = new GildedRose([new Item('Aged Brie', 10, 50)]);
+          const gildedRose = new GildedRose([ItemFactory.createItem('Aged Brie', 10, 50)]);
 
           // Act
           const items = gildedRose.updateQuality();
@@ -65,21 +65,21 @@ describe("GildedRose", () => {
           expect(items[0].quality).toBe(50); // Remains 50
         });
 
-        it("should not decrease quality or sellin of sulfuras item", () => {
+        it("should not decrease quality or sellin of sulfuras item and has fixed quality and sellIn", () => {
           // Arrange
-          const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 10, 20)]);
+          const gildedRose = new GildedRose([ItemFactory.createItem('Sulfuras, Hand of Ragnaros', 10, 20)]);
 
           // Act
           const items = gildedRose.updateQuality();
 
           // Assert
-          expect(items[0].sellIn).toBe(10); // sellIn does not decrease
-          expect(items[0].quality).toBe(20); // quality does not decrease
+          expect(items[0].sellIn).toBe(0); // sellIn does not decrease
+          expect(items[0].quality).toBe(80); // quality does not decrease
         });
 
         it("should increases quality by 1 for backstage passes with sellIn 11 or more", () => {
           // Arrange
-          const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20)]);
+          const gildedRose = new GildedRose([ItemFactory.createItem('Backstage passes to a TAFKAL80ETC concert', 15, 20)]);
 
           // Act
           const items = gildedRose.updateQuality();
@@ -91,7 +91,7 @@ describe("GildedRose", () => {
 
         it("should increases quality by 2 for backstage passes with sellIn 10 or less", () => {
           // Arrange
-          const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20)]);
+          const gildedRose = new GildedRose([ItemFactory.createItem('Backstage passes to a TAFKAL80ETC concert', 10, 20)]);
 
           // Act
           const items = gildedRose.updateQuality();
@@ -103,7 +103,7 @@ describe("GildedRose", () => {
 
       it("should increases quality by 3 for backstage passes with sellIn 5 or less", () => {
         // Arrange
-        const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 20)]);
+        const gildedRose = new GildedRose([ItemFactory.createItem('Backstage passes to a TAFKAL80ETC concert', 5, 20)]);
 
         // Act
         const items = gildedRose.updateQuality();
@@ -115,7 +115,7 @@ describe("GildedRose", () => {
 
       it("should sets quality to 0 for backstage passes after the concert", () => {
         // Arrange
-        const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20)]);
+        const gildedRose = new GildedRose([ItemFactory.createItem('Backstage passes to a TAFKAL80ETC concert', 0, 20)]);
 
         // Act
         const items = gildedRose.updateQuality();
@@ -127,7 +127,7 @@ describe("GildedRose", () => {
 
       it("should never sets quality to negative", () => {
         // Arrange
-        const gildedRose = new GildedRose([new Item('foo', 10, 0)]);
+        const gildedRose = new GildedRose([ItemFactory.createItem('foo', 10, 0)]);
 
         // Act
         const items = gildedRose.updateQuality();
