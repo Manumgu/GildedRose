1b42f1603c69455c82699f466e8c461fb07d2d7f
manuel moreno <manuarchena@gmail.com>
2023-06-12 20:35:54 +0200

    refactor: refactor into if else blocks
    The differentiation of all item types and their respective operations is now effectively done using an if-else block

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -11,59 +11,70 @@ export class Item {
 }
 
 export class GildedRose {
-  items: Array<Item>;
+  private static readonly MAX_QUALITY: number = 50;
+  private items: Array<Item>;
 
   constructor(items = [] as Array<Item>) {
     this.items = items;
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
+    for (let item of this.items) {
+      if (item.name == "Aged Brie") {
+        this.updateBrie(item);
+      } else if (item.name == "Sulfuras, Hand of Ragnaros") {
+        continue;
+      } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+        this.updateBackstagePasses(item);
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
+        this.updateNormal(item);
       }
     }
 
     return this.items;
+  }
+
+  private updateBrie(item: Item) {
+    if (item.quality < GildedRose.MAX_QUALITY) {
+      item.quality += 1;
+    }
+
+    item.sellIn -= 1;
+
+    if (item.sellIn < 0 && item.quality < GildedRose.MAX_QUALITY) {
+      item.quality += 1;
+    }
+  }
+
+  private updateBackstagePasses(item: Item) {
+    if (item.quality < GildedRose.MAX_QUALITY) {
+      item.quality += 1;
+
+      if (item.sellIn <= 10 && item.quality < GildedRose.MAX_QUALITY) {
+        item.quality += 1;
+      }
+
+      if (item.sellIn <= 5 && item.quality < GildedRose.MAX_QUALITY) {
+        item.quality += 1;
+      }
+    }
+
+    item.sellIn -= 1;
+
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    }
+  }
+
+  private updateNormal(item: Item) {
+    if (item.quality > 0) {
+      item.quality -= 1;
+    }
+
+    item.sellIn -= 1;
+
+    if (item.sellIn < 0 && item.quality > 0) {
+      item.quality -= 1;
+    }
   }
 }
